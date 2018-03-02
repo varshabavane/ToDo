bda0cf71131adc81c556db761bddb38623b3121a
varsha bavane <varshab13bavane@gmail.com>
2018-03-02 19:43:39 +0530

    additem and viewItem functions are called at home.ts and also ionviewdidload fnctn created

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+items;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+    this.items = [
+      
+      {title:'hi', description: 'hello' },
+      {title:'hello', description: 'hey' },
+      {title:'hey', description: 'here you are' }
+    ]
+  }
+
+  addItem(){
+    alert("add Item works")
+  }
+
+  viewItem(item){
+    alert("this is your Item" + JSON.stringify(item))
   }
 
 }
