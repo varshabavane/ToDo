9e2062c601b57b4a9373ce00afc77219e4933fa4
varsha bavane <varshab13bavane@gmail.com>
2018-03-08 11:31:15 +0530

    App Testing on Mobile Phone

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 import { AddItemsPage } from '../add-items/add-items'
 import { ItemDetailsPage } from '../item-details/item-details'
-import {DataProvider} from '../../providers/data/data'
+import { DataProvider } from '../../providers/data/data'
 
 @Component({
   selector: 'page-home',
@@ -13,7 +13,7 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, public modalCtrl: ModalController, public data: DataProvider) {
     this.data.getData().then((todos) => {
-      if(todos){
+      if (todos) {
         this.items = todos;
       }
     })
